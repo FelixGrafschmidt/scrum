b7957f82ce7c6152f156352351bf18425b451106
FelixGrafschmidt <felix.grafschmidt@gmail.com>
2023-08-26 21:34:15 +0200

    start work

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-	<div>
-		<NuxtWelcome />
-	</div>
+	<NuxtPage class="h-screen w-screen" />
 </template>
+
+<style>
+	@import "@unocss/reset/tailwind.css";
+	body {
+		@apply bg-gray-8 text-gray-3;
+	}
+</style>
