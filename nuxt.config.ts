16fbabf5204eecc729e1c0a3db4b85c940e034ed
FelixGrafschmidt <felix.grafschmidt@konoma.ch>
2025-03-20 09:04:18 +0100

    rudimentary docker file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: ["@pinia/nuxt", "@unocss/nuxt", "nuxt-icon", "@vueuse/nuxt", "ui.felgraf.dev"],
-	vite: {},
-	app: {
-		head: {
-			title: "Scrum Poker Planner",
-		},
+ devtools: { enabled: true },
+ modules: ["@pinia/nuxt", "@unocss/nuxt", "nuxt-icon", "@vueuse/nuxt", "ui.felgraf.dev"],
+ vite: {},
+
+ app: {
+					head: {
+									title: "Scrum Poker Planner",
+					},
 	},
+
+ compatibilityDate: "2025-03-20",
 });
